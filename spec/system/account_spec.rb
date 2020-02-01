e87c6c7d38a9a55ff21b6eaf7d38d8e142185d94
Justin Mills <MillsJROSS@gmail.com>
2020-02-01 16:17:36 -0600

    wip rubocop

--- spec/system/account_spec.rb
+++ spec/system/account_spec.rb
@@ -1,23 +1,23 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe 'Account' do
-  describe 'registration' do
-    it 'user can register for an account' do
+RSpec.describe "Account" do
+  describe "registration" do
+    it "user can register for an account" do
       visit root_path
 
-      click_on I18n.t('public.landing_page.show.register')
+      click_on I18n.t("public.landing_page.show.register")
 
-      within '#new_user' do
-        fill_in 'user_email', with: 'test@example.com'
-        fill_in 'user_password', with: 'password'
-        fill_in 'user_password_confirmation', with: 'password'
-        click_on I18n.t('public.landing_page.show.register')
+      within "#new_user" do
+        fill_in "user_email", with: "test@example.com"
+        fill_in "user_password", with: "password"
+        fill_in "user_password_confirmation", with: "password"
+        click_on I18n.t("public.landing_page.show.register")
       end
 
       confirmation_path = ActionMailer::Base.deliveries.last.body.match(/"(http:.*)"/)[1]
       visit confirmation_path
 
-      expect(page).to have_selector('.notice', text: I18n.t('devise.confirmations.confirmed'))
+      expect(page).to have_selector(".notice", text: I18n.t("devise.confirmations.confirmed"))
     end
   end
 end
