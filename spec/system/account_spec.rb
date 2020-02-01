2a1c218accf683fb17f860067120fb4e5c6a6649
Justin Mills <MillsJROSS@gmail.com>
2020-02-01 15:22:45 -0600

    gem devise configuration

--- spec/system/account_spec.rb
+++ spec/system/account_spec.rb
@@ -4,6 +4,12 @@ RSpec.describe 'Account' do
   describe 'registration' do
     it 'user can register for an account' do
       visit root_path
+
+      click_on I18n.t('public.landing_page.show.register')
+
+      within '#new_user' do
+
+      end
     end
   end
 end
