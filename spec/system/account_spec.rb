c723b998cedf4a178db485a545ece2cfddd74090
Justin Mills <MillsJROSS@gmail.com>
2020-02-01 15:57:36 -0600

    wip user registration

--- spec/system/account_spec.rb
+++ spec/system/account_spec.rb
@@ -8,8 +8,16 @@ RSpec.describe 'Account' do
       click_on I18n.t('public.landing_page.show.register')
 
       within '#new_user' do
-
+        fill_in 'user_email', with: 'test@example.com'
+        fill_in 'user_password', with: 'password'
+        fill_in 'user_password_confirmation', with: 'password'
+        click_on I18n.t('public.landing_page.show.register')
       end
+
+      confirmation_path = ActionMailer::Base.deliveries.last.body.match(/"(http:.*)"/)[1]
+      visit confirmation_path
+
+      expect(page).to have_selector('.notice', text: I18n.t('devise.confirmations.confirmed'))
     end
   end
 end
