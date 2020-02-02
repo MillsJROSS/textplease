60db9977494c8a0eccff21a9fecbfd87be9c1a9f
Justin Mills <MillsJROSS@gmail.com>
2020-02-02 09:38:28 -0600

    updated user model testing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,5 +25,20 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#email' do
+    it 'must be present' do
+      user = build(:user, email: '')
+
+      expect(user).not_to be_valid
+      expect(user.errors[:email]).to include(t('errors.messages.blank'))
+    end
+
+    it 'must be unique' do
+      first_user = create(:user, email: 'user@example.com')
+      second_user = build(:user, email: 'user@example.com')
+
+      expect(second_user).not_to be_valid
+      expect(second_user.errors[:email]).to include(t('errors.messages.taken'))
+    end
+  end
 end
